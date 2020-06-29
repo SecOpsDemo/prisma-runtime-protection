92a94eb8d44b6b088cbc0845ade5a06b5996a29e
Changyu Seon (선찬규) <changyu.seon@bespinglobal.com>
2020-06-29 12:53:03 +0900

    add subprocess function

---
+++ subprocess/app.py
@@ -0,0 +1,33 @@
+# ---------------------------------------------------------
+# Author:  Matthew Barker, mbarker@paloaltonetworks.com
+#
+# To protect this lambda function with Prisma Cloud Compute:
+#   - create policy in Defend/Runtime/Serverless
+#   - create policy in Defend/Firewalls/CloudNativeAppFirewall/Serverless
+#   - download python based serverless defender as zip file here and unzip
+#   - remove the comments for the 2 lines with 'twistlock' in them
+#   - zip up everything but the zip file (use -r option) and upload zip file to your new lambda function in AWS
+# ----------------------------------------------------------
+
+import subprocess
+import os
+import json
+
+def lambda_handler(event, context):
+    cmd = "echo hello subprocess."
+    if "cmd" in event:
+        cmd = event["cmd"]
+
+    try:
+        result =  subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout
+        out =  result.read()
+        out = out.decode("utf8")
+    except Exception as e:
+        out = str(e)
+
+    return {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": {"Content-Type": "text/plain"},
+        "body": out
+    }
